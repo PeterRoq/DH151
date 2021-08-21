bac43dc7ec803352fa4f6832ec55bcb86955b320
PeterRoq <proquemore@luskin.ucla.edu>
2021-08-21 09:04:21 -0700

    lastdate

--- Week 5/js/map.js
+++ Week 5/js/map.js
@@ -6,6 +6,7 @@ let zl = 3;
 let path = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
 let markers = L.featureGroup();
 let csvdata;
+let lastdate;
 
 // initialize
 $( document ).ready(function() {
@@ -31,25 +32,39 @@ function readCSV(path){
 			console.log(data);
             csvdata = data;
 
-            mapCSV(data);
+            //get the last date and put it in the global variable 
+            lastdate = ccsvdata.meta.fields[csvdata.meta.fields.length-1];
+
+            mapCSV(lastdate);
 			
 		}
 	});
 }
-function mapCSV(){
+function mapCSV(date){
+
+    //clear layers in case you are calling this function more than once
+    markers.clearLayers();
 
 	// loop through every row in the csv data
 	csvdata.data.forEach(function(item,index){
 		// check to make sure the Latitude column exists
 		if(item.Lat != undefined){
-
+            let circleOptions = {
+            radius: item[date]/32000, //divide by a higher number to get usable circle sizes
+            weight: 1,
+            color: 'white',
+            fillColor: 'red',
+            fillOpacity: 0.5
+        }
+            
 			// Lat exists, so create a circleMarker for each country
-            let marker = L.circlemarker([item.Lat,item.Long],cirlceoptions)
-
-			// add the circleMarker to the featuregroup
-            markers.addLayer(marker)
-		} // end if
-	})
+            let marker = L.circleMarker([item.Lat,item.Long],circleOptions)
+			.on('mouseover',function(){
+				this.bindPopup(`${item['Country/Region']}<br>Total confirmed cases as of ${date}: ${item[date]}`).openPopup()
+			}) // show data on hover
+			markers.addLayer(marker)	
+		}   
+	});
 
 	// add the featuregroup to the map
             markers.addTo(map)
